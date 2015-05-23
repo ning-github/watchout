88f06134dbd256720f9fe662462f522d8ea7bbba
ning-github <nxiamail@gmail.com>
2015-05-22 19:57:01 -0700

    working on tweening for collisions

--- watchout.js
+++ watchout.js
@@ -4,6 +4,7 @@ var boardWidth = 500;
 var boardHeight = 500;
 var nEnemies = 15;
 var enemyData = [];
+var playerRadius = 20;
 
 var gameBoard = d3.select('body').append('svg:svg')
   .attr('class', 'gameBoard')
@@ -33,6 +34,33 @@ enemies.enter().append('svg:circle')
   .attr('cy', function(d) { return d.y; })
   .attr('r', 6)
   .attr('fill', 'red');
+
+
+/* player's dot */
+/*------------------------------------------------*/
+var onlyPlayer = {x:250, y:250};
+var dragmove = function(d){
+  d3.select(this)
+  .attr("cx", d.x = Math.max(playerRadius, Math.min(boardWidth - playerRadius, d3.event.x)))
+  .attr("cy", d.y = Math.max(playerRadius, Math.min(boardHeight - playerRadius, d3.event.y)));
+};
+
+var drag = d3.behavior.drag()
+  .origin(function(d){return d;})
+  .on('drag', dragmove);
+
+var player = gameBoard.selectAll('circle.player')
+  .data([onlyPlayer])
+  .call(drag);
+
+player.enter().append('svg:circle')
+  .attr('class','player')
+  .attr('cx', function(d) { return d.x; })
+  .attr('cy', function(d) { return d.y; })
+  .attr('r', playerRadius)
+  .attr('fill', 'white');
+
+player.call(drag);
 
 /* random movement */
 /*------------------------------------------------*/
@@ -68,34 +96,26 @@ var moving = function(){
 }
 d3.timer(moving(), 1000);
 
-/* player's dot */
+/* collision detection */
 /*------------------------------------------------*/
-var width = boardWidth;
-var height = boardHeight;
-var radius = 6;
 
-var dragmove = function(d){
-  d3.select(this)
-  .attr("cx", d.x = Math.max(radius, Math.min(width - radius, d3.event.x)))
-  .attr("cy", d.y = Math.max(radius, Math.min(height - radius, d3.event.y)));
+var onCollision = function() {
+  // updateBestScore();
+  // updateScore();
+  // set current score to zero
 };
 
-var drag = d3.behavior.drag()
-  .origin(function(d){return d;})
-  .on('drag', dragmove);
-
-
-var player = gameBoard.selectAll('circle.player')
-  .data([{x:250, y:250}])
-  .call(drag);
-
-player.enter().append('svg:circle')
-  .attr('class','player')
+gameBoard.selectAll('circle.enemy')
+  .data(enemyData).transition().duration(1000)
   .attr('cx', function(d) { return d.x; })
   .attr('cy', function(d) { return d.y; })
-  .attr('r', 20)
-  .attr('fill', 'white');
+  .tween('custom',function(eachEnemy){
+    //check if that enemy collided
+    var radiusSum = parseFloat(eachEnemy.attr('r')) + player.attr('r');
+    var xDelta = parseFloat(eachEnemy.attri('cx')) + player.attr('cx');
+    var yDelta = parseFloat(eachEnemy.attri('cy')) + player.attr('cy');
 
+    var distanceDelta = Math.sqrt( Math.pow(xDelta, 2) + Math.pow(yDelta, 2));
+    if (distanceDelta < radiusSum) null;// write some callback here
+  });
 
-
-player.call(drag);
