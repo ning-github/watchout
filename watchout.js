02ab824652a4754a52a5be3684fc6ec1d2bb35ca
ning-github <nxiamail@gmail.com>
2015-05-22 19:06:37 -0700

    - added attr for new coordniates to actually see the DOM shift to reflect them - d3.timer using function instance - made a draggable player

--- watchout.js
+++ watchout.js
@@ -24,26 +24,78 @@ var generateEnemies = function(array) {
 
 generateEnemies(enemyData);
 
-var enemies = gameBoard.selectAll('circle')
+var enemies = gameBoard.selectAll('circle.enemy')
   .data(enemyData);
 
 enemies.enter().append('svg:circle')
+  .attr('class','enemy')
   .attr('cx', function(d) { return d.x; })
   .attr('cy', function(d) { return d.y; })
   .attr('r', 6)
   .attr('fill', 'red');
 
+/* random movement */
 /*------------------------------------------------*/
-debugger;
 
-for( var i = 0; i < enemyData.length; i++ ){
-  var eachEnemy = enemyData[i];
+var moveOnce = function(eachEnemy){
   for( var key in eachEnemy ) {
     eachEnemy[key] = Math.random() * 500;
   }
+  return eachEnemy;
 }
 
-gameBoard.selectAll('circle')
-  .data(enemyData, function(d){return d;}).transition().duration(2000);
+for (var j = 0; j < enemyData.length; j++){
+  moveOnce(enemyData[j]);
+}
+
+gameBoard.selectAll('circle.enemy')
+  .data(enemyData).transition().duration(1000)
+  .attr('cx', function(d) { return d.x; })  // NEED to reset DOM node coords
+  .attr('cy', function(d) { return d.y; });   // so that the DOM can REFLECT its new coords and not just have them set
+
+var moving = function(){
+  return function(){
+    for (var j = 0; j < enemyData.length; j++){
+      moveOnce(enemyData[j]);
+    }
+    gameBoard.selectAll('circle.enemy')
+      .data(enemyData).transition().duration(1000)
+      .attr('cx', function(d) { return d.x; })
+      .attr('cy', function(d) { return d.y; });
+    d3.timer(moving(), 1000);
+    return true;
+  }
+}
+d3.timer(moving(), 1000);
+
+/* player's dot */
+/*------------------------------------------------*/
+var width = boardWidth;
+var height = boardHeight;
+var radius = 6;
+
+var dragmove = function(d){
+  d3.select(this)
+  .attr("cx", d.x = Math.max(radius, Math.min(width - radius, d3.event.x)))
+  .attr("cy", d.y = Math.max(radius, Math.min(height - radius, d3.event.y)));
+};
+
+var drag = d3.behavior.drag()
+  .origin(function(d){return d;})
+  .on('drag', dragmove);
 
 
+var player = gameBoard.selectAll('circle.player')
+  .data([{x:250, y:250}])
+  .call(drag);
+
+player.enter().append('svg:circle')
+  .attr('class','player')
+  .attr('cx', function(d) { return d.x; })
+  .attr('cy', function(d) { return d.y; })
+  .attr('r', 20)
+  .attr('fill', 'white');
+
+
+
+player.call(drag);
